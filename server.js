45032b45ee4a87f8b344386fb062abb0e39d50fc
Nikhil Sharma <nikhil_sharma@live.co.uk>
2018-06-29 13:10:55 +0100

    Initial commit

---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const app = express();
+const parser = require("body-parser");
+
+const port = 3001;
+
+app.use(parser.json());
+app.use(parser.urlencoded({ extended: true }));
+
+const MongoClient = require("mongodb").MongoClient;
+
+MongoClient.connect(
+  "mongodb://localhost:27017",
+  (err, client) => {
+    if (err) next(err);
+    const db = client.db("historyDB");
+    console.log("Connected to db...");
+
+    app.get("/api/history", (req, res, next) => {
+      const historyCollection = db.collection("rome");
+      historyCollection.find().toArray((err, history) => {
+        if (err) next(err);
+        res.json(history);
+      });
+    });
+
+    app.post("/api/history", (req, res, next) => {
+      const historyCollection = db.collection("rome");
+      console.log(req.body);
+      const newEvent = req.body;
+      historyCollection.insert(newEvent, (err, result) => {
+        if (err) next(err);
+        res.status(201);
+        res.json(result.ops[0]);
+      });
+    });
+
+    app.listen(port, () => {
+      console.log("App listening on port", port);
+    });
+  }
+);
