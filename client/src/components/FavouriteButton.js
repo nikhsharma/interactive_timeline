16bf01e35e7e0f249d8377fc11a1c9c805157e9e
Scott Stevenson <scotifer92@googlemail.com>
2018-07-01 13:19:25 +0100

    Favourite button can be clicked

--- client/src/components/FavouriteButton.js
+++ client/src/components/FavouriteButton.js
@@ -13,10 +13,15 @@ export default class FavouriteButton extends Component {
     console.log("Favourite Button mounted");
   }
 
+  handleFavClick(event) {
+    // event.preventDefault();
+    console.log("Favourite Button was clicked");
+  }
+
   render() {
     return (
       <div className="save-button">
-        <button type="submit">
+        <button onClick={this.handleFavClick} type="submit">
           <img src="https://cdn3.iconfinder.com/data/icons/linecons-free-vector-icons-pack/32/star-512.png" width="19" height="15" alt="Save" />
         </button>
       </div>
