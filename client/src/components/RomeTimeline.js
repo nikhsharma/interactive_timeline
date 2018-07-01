694efec6c6c80a787814e091091e9e7eca30cefd
Nikhil Sharma <nikhil_sharma@live.co.uk>
2018-07-01 14:39:30 +0100

    Add button to change scroll position

--- client/src/components/RomeTimeline.js
+++ client/src/components/RomeTimeline.js
@@ -1,5 +1,4 @@
 import React, {Component} from 'react';
-import {Link} from "react-router-dom";
 import './RomanTimeline.css'
 
 export default class RomeTimeline extends Component {
@@ -16,30 +15,32 @@ export default class RomeTimeline extends Component {
     fetch(url).then(res => res.json()).then(data => this.setState({data: data['0'].data}))
     // this.pageScroll();
     setInterval(this.scrollDiv,50)
+    const button = document.querySelector('.move-button')
+    button.addEventListener('click', () => {document.querySelector('.timeline').scrollLeft = 4700})
   }
 
   scrollDiv(){
-      if (!document.querySelector('.timeline')) return null;
-
-      if(document.querySelector('.timeline').scrollLeft<(document.querySelector('.timeline').scrollWidth-document.querySelector('.timeline').offsetWidth)){-1
-        document.querySelector('.timeline').scrollLeft=document.querySelector('.timeline').scrollLeft+1
+    const timeline = document.querySelector('.timeline');
+      if (!timeline) return null;
+      if(timeline.scrollLeft<(timeline.scrollWidth-timeline.offsetWidth)){
+        timeline.scrollLeft=timeline.scrollLeft+1
+        // console.log(timeline.scrollLeft);
             }
       else {document.querySelector('.timeline').scrollTop=0;}
     }
 
 
   render() {
+
     if (!this.state.data) return null;
     const events = this.state.data.map( event => (
-        <div className="event">
-          {/* <li  key={event.data_date}> */}
+        <div key={event.data_date} className="event">
             <div className="date">
               {event.data_date}
             </div>
             <div className="content ">
               {event.content}
             </div>
-          {/* </li> */}
         </div>
     ))
     return  (
@@ -47,6 +48,7 @@ export default class RomeTimeline extends Component {
       <div className='timeline'>
         <div className='hline'></div>
         {events}
+        <button className='move-button'>Move timeline</button>
       </div>
       </div>
     );
--- seeds.js
+++ seeds.js
@@ -33,73 +33,73 @@ db.rome.insert({ data: [
     data_date: -616,
     content: "The Curiate Assembly elected Lucius Tarquinius Priscus King of Rome."
   }, {
-    data_date: -753,
+    data_date: -7531,
     content: "21 April 	Rome was founded."
   },
 
   {
-    data_date: -673,
+    data_date: -6731,
     content: "Tullus Hostilius became King of Rome."
   },
 
   {
-    data_date: -667,
+    data_date: -6671,
     content: "Byzantium was founded by Megarian colonists."
   },
 
   {
-    data_date: -642,
+    data_date: -6421,
     content: "Tullus Hostilius died. 	The Curiate Assembly, one of the legislative assemblies of the Roman Kingdom, elected Ancus Marcius King of Rome."
   },
 
   {
-    data_date: -617,
+    data_date: -6171,
     content: "Ancus Marcius died."
   },{
-    data_date: -753,
+    data_date: -7532,
     content: "21 April 	Rome was founded."
   },
 
   {
-    data_date: -673,
+    data_date: -6732,
     content: "Tullus Hostilius became King of Rome."
   },
 
   {
-    data_date: -667,
+    data_date: -6672,
     content: "Byzantium was founded by Megarian colonists."
   },
 
   {
-    data_date: -642,
+    data_date: -6422,
     content: "Tullus Hostilius died. 	The Curiate Assembly, one of the legislative assemblies of the Roman Kingdom, elected Ancus Marcius King of Rome."
   },
 
   {
-    data_date: -617,
+    data_date: -6172,
     content: "Ancus Marcius died."
   },{
-    data_date: -753,
+    data_date: -7533,
     content: "21 April 	Rome was founded."
   },
 
   {
-    data_date: -673,
+    data_date: -6733,
     content: "Tullus Hostilius became King of Rome."
   },
 
   {
-    data_date: -667,
+    data_date: -6673,
     content: "Byzantium was founded by Megarian colonists."
   },
 
   {
-    data_date: -642,
+    data_date: -6423,
     content: "Tullus Hostilius died. 	The Curiate Assembly, one of the legislative assemblies of the Roman Kingdom, elected Ancus Marcius King of Rome."
   },
 
   {
-    data_date: -617,
+    data_date: -6173,
     content: "Ancus Marcius died."
   }
 ]});
